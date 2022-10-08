e7becf3572a47da3288967ad49e96a65d8f9037f
Josue A. Rojas Paredes <paredesjosue77@gmail.com>
2022-10-08 16:53:15 -0500

    Actualizando cards

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,21 +1,71 @@
-import { useState } from 'react';
+import {   IonCard, IonCardContent, IonCardHeader,  IonCol,  IonFooter,  IonIcon, IonRow,  IonSpinner, IonTitle } from '@ionic/react';
 import useFetchPokemon from '../hooks/useFetchPokemon';
 import './ExploreContainer.css';
+import {statsChartOutline ,heartOutline, diamondOutline} from 'ionicons/icons'
 
 interface Pokemon { 
   name:string,
-  url:string
+  sprites:{
+    other:{
+      dream_world:{
+        front_default:string
+      }
+    }
+  }
+  id:number
+  types:Types[]
+  stats:Stats[]
+}
+
+interface Types{
+  type:{
+    name:string
+  }
+}
+
+interface Stats {
+  base_stat:number,
 }
 
 const ExploreContainer: React.FC = () => {
-  const [render, setRender] = useState(null)
-  const arrPokemons = useFetchPokemon(setRender);
+
+  const arrPokemons = useFetchPokemon();
   return (
-    <div className="container">
-      {
-        render ? arrPokemons.map((pokemon:Pokemon)=>(<p key={pokemon.name}>{pokemon.name}</p> )):<h1>Cargando</h1>
-      }
-    </div>
+     <>
+        {
+          arrPokemons.length > 0 ? (
+            arrPokemons.map((pokemon:Pokemon) => (
+              <IonCard button>
+              <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} className="imgPokeCard" />
+              <IonCardHeader>
+                <IonTitle>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</IonTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                  <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor N. del T. <span className='info'>Ver mas...</span> </p>
+              </IonCardContent>
+
+              <IonFooter>
+                <IonRow className='card_footer'>
+                  <IonCol>
+                      <IonIcon icon={heartOutline} size="large"/>
+                      <p>HP: {pokemon.stats[0].base_stat}</p>
+                  </IonCol>
+                  <IonCol>
+                      <IonIcon icon={statsChartOutline} size="large"/>
+                      <p>Attack: {pokemon.stats[1].base_stat}</p>
+                  </IonCol>
+                  <IonCol>
+                      <IonIcon icon={diamondOutline} size="large"/>
+                      <p>Defense: {pokemon.stats[2].base_stat}</p>
+                  </IonCol>
+                </IonRow>
+              </IonFooter>
+              
+            </IonCard>
+            ))
+          ): <IonSpinner/>
+        }
+     </>
   );
 };
 
--- src/hooks/useFetchPokemon.jsx
+++ src/hooks/useFetchPokemon.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
 
-const useFetchPokemon = (setRender) => {
+const useFetchPokemon = () => {
   const [arrPokemons, setPokemons] = useState([]);
   useEffect(() => {
     async function fectPokemon() {
+      const arr = [];
       try {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon");
-        const { results } = await res.json();
-        setPokemons(results);
-        setRender(true);
+        for (let index = 1; index <= 20; index++) {
+          const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
+          const data = await res.json();
+          arr.push(data);
+          setPokemons([...arr]);
+        }
       } catch (error) {
         setPokemons([]);
-        setRender(null);
       }
     }
 
     fectPokemon();
-  }, [setRender]);
+  }, []);
 
   return arrPokemons;
 };
